097d138a4818defbab486e3741e0351f10d988d8
Cassio Glay <cassioglay@gmail.com>
2021-04-26 17:51:03 -0300

    Config. Redux

---
+++ src/main/reducers.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler livro',
+        list: [{
+            _id: 1,
+            description: 'Pagar fatura de cartao',
+            done: true
+        }, {
+            _id: 2,
+            description: 'Reuniao com a equipe a 10:00',
+            done: true
+        },
+        {
+            _id: 3,
+            description: 'Consulta medica na terca depois do almoco',
+            done: true
+        }]
+    })
+});
+
+export default rootReducer;
